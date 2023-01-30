dcff7895d5fd2dd9bb8c7df77c2fee828d65dcfb
PortalFan202 <anibaldonoso1@gmail.com>
2023-01-30 10:34:17 +0200

    Finished project

--- app/src/main/java/com/example/farenheitcelsius/MainActivity.kt
+++ app/src/main/java/com/example/farenheitcelsius/MainActivity.kt
@@ -3,15 +3,16 @@ package com.example.farenheitcelsius
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,15 +23,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FarenheitCelsiusTheme {
-                FarenheitCelsius()
+                FahrenheitCelsius()
             }
         }
     }
 }
 
 @Composable
-fun FarenheitCelsius() {
+fun FahrenheitCelsius() {
     var temp: String by remember { mutableStateOf("") }
+    var fahrenheitSelected: Boolean by remember { mutableStateOf(true) }
+    val tempFloat: Float = temp.toFloatOrNull() ?: 0.0f
+    val result = when (fahrenheitSelected) {
+        true -> if (tempFloat > 0.0f) (tempFloat - 32) / 1.8f else 0.0f
+        false -> if (tempFloat > 0.0f) (tempFloat * 1.8f) + 32 else 0.0f
+    }
+
     Column(
         modifier = Modifier.padding(8.dp)
     ) {
@@ -39,12 +47,34 @@ fun FarenheitCelsius() {
             color = MaterialTheme.colors.primary,
             fontSize = 24.sp,
             textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
         )
+
         OutlinedTextField(
             value = temp,
             onValueChange = { temp = it },
-            label = { Text(text = stringResource(R.string.input_text))}
+            label = { Text(text = stringResource(R.string.input_text)) },
+            textStyle = TextStyle(color = MaterialTheme.colors.primary),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(selected = fahrenheitSelected, onClick = { fahrenheitSelected = true })
+            Text(text = stringResource(R.string.radio_one))
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(selected = !fahrenheitSelected, onClick = { fahrenheitSelected = false })
+            Text(text = stringResource(R.string.radio_two))
+        }
+        Text(
+            text = String.format("%.2f", result),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
         )
     }
 }
