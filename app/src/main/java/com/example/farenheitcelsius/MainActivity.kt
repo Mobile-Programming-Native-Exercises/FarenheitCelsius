1db358440ee1b483ddce0268bade2cbe8c2752eb
PortalFan202 <anibaldonoso1@gmail.com>
2023-01-30 10:14:00 +0200

    Added base UI

--- app/src/main/java/com/example/farenheitcelsius/MainActivity.kt
+++ app/src/main/java/com/example/farenheitcelsius/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.farenheitcelsius
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.farenheitcelsius.ui.theme.FarenheitCelsiusTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +22,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FarenheitCelsiusTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                FarenheitCelsius()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FarenheitCelsiusTheme {
-        Greeting("Android")
+fun FarenheitCelsius() {
+    var temp: String by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.title),
+            color = MaterialTheme.colors.primary,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = temp,
+            onValueChange = { temp = it },
+            label = { Text(text = stringResource(R.string.input_text))}
+        )
     }
 }
